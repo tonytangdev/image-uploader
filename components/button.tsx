08f0eca630fe3283e47603db44bd28859ead1836
tonytangdev <tony.tang.developer@gmail.com>
2022-02-26 13:07:41 +0100

    Test prettier

--- components/button.tsx
+++ components/button.tsx
@@ -1,13 +1,16 @@
-import { MouseEventHandler } from "react"
+import { MouseEventHandler } from 'react'
 
 export interface ButtonProps {
-  onPress: MouseEventHandler<HTMLButtonElement>,
+  onPress: MouseEventHandler<HTMLButtonElement>
   label: string
 }
 
 const Button = (props: ButtonProps) => {
   return (
-    <button onClick={props.onPress} className="font-['Noto_Sans'] text-xs font-medium bg-[#2F80ED] text-white px-4 py-2 rounded-lg active:bg-opacity-80">
+    <button
+      onClick={props.onPress}
+      className="font-['Noto_Sans'] text-xs font-medium bg-[#2F80ED] text-white px-4 py-2 rounded-lg active:bg-opacity-80"
+    >
       {props.label}
     </button>
   )
