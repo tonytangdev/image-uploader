a98912b24bb6636152994af215f6402126f03ba3
tonytangdev <tony.tang.developer@gmail.com>
2022-02-28 21:39:47 +0100

    Add hover state to button

--- components/button.tsx
+++ components/button.tsx
@@ -9,7 +9,7 @@ const Button = ({ onPress, label }: ButtonProps) => {
   return (
     <button
       onClick={onPress}
-      className="font-['Noto_Sans'] text-xs font-medium bg-[#2F80ED] text-white px-4 py-2 rounded-lg active:bg-opacity-80 inline-block whitespace-nowrap"
+      className="font-['Noto_Sans'] text-xs font-medium bg-[#2F80ED] text-white px-4 py-2 rounded-lg active:bg-opacity-50 hover:bg-opacity-90 inline-block whitespace-nowrap"
     >
       {label}
     </button>
